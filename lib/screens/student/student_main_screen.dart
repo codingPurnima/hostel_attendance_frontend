c6dd2be1f02819547f26d0c3e59a696ad4d36247
Purnima Pandey <purnimapnd04@gmail.com>
2026-05-25 14:07:02 +0530

    Fetch token from AuthService and pass to MainScreen

--- lib/screens/student/student_main_screen.dart
+++ lib/screens/student/student_main_screen.dart
@@ -4,19 +4,32 @@ import 'package:hostel_attendance_frontend/screens/student/leave_screen.dart';
 import 'package:hostel_attendance_frontend/screens/student/show_history.dart';
 import 'package:hostel_attendance_frontend/screens/student/show_profile.dart';
 import 'package:hostel_attendance_frontend/screens/student/student_home.dart';
+import 'package:hostel_attendance_frontend/services/auth_service.dart';
 
 class StudentMainScreen extends StatelessWidget {
   const StudentMainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final token = AuthService.accessToken!;
+
     return MainScreen(
-      screens: const [StudentHome(), HistoryScreen(), LeaveScreen(), ProfileScreen()], navItems: const [
+      token: token,
+      screens: [
+        StudentHome(),
+        HistoryScreen(),
+        LeaveScreen(token: token),
+        ProfileScreen(),
+      ],
+      navItems: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
         BottomNavigationBarItem(icon: Icon(Icons.history), label: "History"),
-        BottomNavigationBarItem(icon: Icon(Icons.exit_to_app_rounded), label: "Leave"),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.exit_to_app_rounded),
+          label: "Leave",
+        ),
         BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-      ]
+      ],
     );
   }
 }
