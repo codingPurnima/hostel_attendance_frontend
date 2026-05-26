1573964d41392d53605ce421f9ee1038f2c7b2a5
Purnima Pandey <purnimapnd04@gmail.com>
2026-05-26 12:05:55 +0530

    Add callback to cards on tap

--- lib/widgets/build_leave_card.dart
+++ lib/widgets/build_leave_card.dart
@@ -22,52 +22,56 @@ Color getStatusColor(String status) {
 
 class LeaveCard extends StatelessWidget {
   final LeaveModel leave;
+  final VoidCallback? onTap;
 
-  const LeaveCard({super.key, required this.leave});
+  const LeaveCard({super.key, required this.leave, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      child: Padding(
-        padding: const EdgeInsets.all(14),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Leave for ${leave.reason}",
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 5,
-                  ),
-                  decoration: BoxDecoration(
-                    color: getStatusColor(leave.status).withAlpha(38),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Text(
-                    leave.status,
-                    style: TextStyle(
-                      color: getStatusColor(leave.status),
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        child: Padding(
+          padding: const EdgeInsets.all(14),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Leave for ${leave.reason}",
+                    style: const TextStyle(
+                      fontSize: 18,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 12),
-            Text("Start Date: ${leave.startDate}"),
-            const SizedBox(height: 6),
-            Text("End Date: ${leave.endDate}"),
-          ],
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 5,
+                    ),
+                    decoration: BoxDecoration(
+                      color: getStatusColor(leave.status).withAlpha(38),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      leave.status,
+                      style: TextStyle(
+                        color: getStatusColor(leave.status),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12),
+              Text("Start Date: ${leave.startDate}"),
+              const SizedBox(height: 6),
+              Text("End Date: ${leave.endDate}"),
+            ],
+          ),
         ),
       ),
     );
