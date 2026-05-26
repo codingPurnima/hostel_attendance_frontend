2aedbc3e93a626dbc68e120968e67997a420f4f7
Purnima Pandey <purnimapnd04@gmail.com>
2026-05-26 22:23:53 +0530

    Replace hardcoded base URLs with ApiService.baseUrl

--- lib/services/leave_service.dart
+++ lib/services/leave_service.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
+import 'package:hostel_attendance_frontend/services/api_service.dart';
 import 'package:http/http.dart' as http;
 
 class LeaveService {
-  static const String baseUrl = "http://10.0.2.2:8000";
+  static const String baseUrl = ApiService.baseUrl;
 
   static Future<Map<String, dynamic>> requestLeave({
     required String token,
