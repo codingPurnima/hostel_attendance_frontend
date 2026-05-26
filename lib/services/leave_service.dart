338a97c4969068fca4467891d2bd422946ef7854
Purnima Pandey <purnimapnd04@gmail.com>
2026-05-26 11:27:07 +0530

    Create leave request

--- lib/services/leave_service.dart
+++ lib/services/leave_service.dart
@@ -4,36 +4,61 @@ import 'package:http/http.dart' as http;
 class LeaveService {
   static const String baseUrl = "http://10.0.2.2:8000";
 
+  static Future<Map<String, dynamic>> requestLeave({
+    required String token,
+    required String startDate,
+    required String endDate,
+    required String reason,
+  }) async {
+    try {
+      final response = await http.post(
+        Uri.parse("$baseUrl/leave/leave-request"),
+
+        headers: {
+          "Authorization": "Bearer $token",
+          "Content-Type": "application/json",
+        },
+
+        body: jsonEncode({
+          "start_date": startDate,
+          "end_date": endDate,
+          "reason": reason,
+        }),
+      );
+
+      final data = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        return {"success": true, "data": data};
+      } else {
+        return {
+          "success": false,
+          "message": data["detail"] ?? "Failed to request leave",
+        };
+      }
+    } catch (e) {
+      return {"success": false, "message": e.toString()};
+    }
+  }
+
   static Future<Map<String, dynamic>> getMyLeaves({
     required String token,
   }) async {
     try {
       final response = await http.get(
         Uri.parse("$baseUrl/leave/my-leaves"),
-        headers: {
-          "Authorization": "Bearer $token",
-        },
+        headers: {"Authorization": "Bearer $token"},
       );
 
       final data = jsonDecode(response.body);
 
       if (response.statusCode == 200) {
-        return {
-          "success": true,
-          "data": data,
-        };
+        return {"success": true, "data": data};
       } else {
-        return {
-          "success": false,
-          "message": "Failed to fetch leaves",
-        };
+        return {"success": false, "message": "Failed to fetch leaves"};
       }
     } catch (e) {
-      return {
-        "success": false,
-        "message": e.toString(),
-      };
+      return {"success": false, "message": e.toString()};
     }
-  }   
-
+  }
 }
