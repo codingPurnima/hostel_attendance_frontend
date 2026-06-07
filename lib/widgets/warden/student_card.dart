b221020e0a789ed90101e9c94719129930e0c6b0
Purnima Pandey <purnimapnd04@gmail.com>
2026-06-07 11:21:43 +0530

    Show student details

---
+++ lib/widgets/warden/student_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class StudentsCard extends StatelessWidget {
+  final List<dynamic> students;
+  const StudentsCard({super.key, required this.students});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Students",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+
+          const SizedBox(height: 20),
+
+          DataTable(
+            columns: const [
+              DataColumn(label: Text("ID")),
+              DataColumn(label: Text("Room")),
+              DataColumn(label: Text("Email")),
+              DataColumn(label: Text("Status")),
+            ],
+            rows: students.map((student) {
+
+              return DataRow(
+                cells: [
+
+                  DataCell(
+                    Text(student["id"].toString()),
+                  ),
+
+                  DataCell(
+                    Text(student["room"].toString()),
+                  ),
+
+                  DataCell(
+                    Text(student["email"].toString()),
+                  ),
+
+                  DataCell(
+                    Text(student["status"].toString()),
+                  ),
+                ],
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
